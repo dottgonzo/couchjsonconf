96aaaf9e9c7d5b327b845780e8e3d446c80cbcad
Dario Caruso <darioyzf@gmail.com>
2015-10-10 16:38:00 +0200

    uri

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+var url=require('url')
+
 module.exports = function(json) {
   if(json.user&&json.password){
     json.url=json.protocol+'://'+json.user+':'+json.password+'@'+json.domain+':'+json.port;
     json.data=json.protocol+'://'+json.user+':'+json.password+'@'+json.domain+':'+json.port+'/'+json.database;
-
+json.uri=url(json.data)
   } else{
     json.url=json.protocol+'://'+json.domain+':'+json.port;
     json.data=json.protocol+'://'+json.domain+':'+json.port+'/'+json.database;
-
+    json.uri=url(json.data)
   }
 
 return json
