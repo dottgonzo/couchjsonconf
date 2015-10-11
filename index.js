c5cf4abcba24979df8e88aa212446eda33e771b0
Dario Caruso <darioyzf@gmail.com>
2015-10-11 11:41:44 +0200

    oki

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict';
 var URL=require('url-parse');
 module.exports = function(json) {
+
   if(json.user&&json.password){
-    var url=new URL(json.protocol+'://'+json.user+':'+json.password+'@'+json.domain+':'+json.port+'/'+json.database);
+    var url=new URL(json.protocol+'://'+json.user+':'+json.password+'@'+json.domain+':'+json.port+'/');
       } else{
 
-    var url=new URL(json.protocol+'://'+json.domain+':'+json.port+'/'+json.database);
+    var url=new URL(json.protocol+'://'+json.domain+':'+json.port+'/');
 
   }
 
